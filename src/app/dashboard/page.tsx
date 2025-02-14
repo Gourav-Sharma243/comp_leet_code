29c4ae26c5c1c5e7d01b8d99be7c6e0aee4ef57a
prathamgarg03 <gargpratham.312003@gmail.com>
2025-02-14 13:33:23 -0800

    Create create-submission-token and get-submission server actions

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,7 +1,35 @@
+"use client"
+
+import {useState} from "react";
+import {CreateSubmissionToken} from "@/actions/create-submission-token";
+import {GetSubmission} from "@/actions/get-submission";
+
 export default function DashboardPage() {
+    const [sourceCode, setSourceCode] = useState("");
+
+    async function onCLick() {
+        // console.log(sourceCode);
+        console.log("clicked")
+
+        const token = await CreateSubmissionToken("print(2+3)", "5");
+
+
+        console.log(token);
+
+
+        // const data = await GetSubmission(token);
+        //
+        // console.log(data);
+    }
+
     return (
         <div>
-            Dashboard Page
+            <h1>Dashboard</h1>
+            <textarea
+                value={sourceCode}
+                onChange={(e) => setSourceCode(e.target.value)}
+            />
+            <button onClick={onCLick}>Submit</button>
         </div>
     );
 }
